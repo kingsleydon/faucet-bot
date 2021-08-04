7503fd8bcc9e4e8bba2e0e975f485cb0bf6d5875
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2021-08-04 18:37:31 +0800

    chore: upgrade deps

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,30 +2,6 @@
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-# Mnenonic of the account (sr25516)
-export MNEMONIC='rail return lawsuit bachelor dash ozone way common tired trumpet similar glad'
-# Substrate websocket endpoint
-export SUBSTRATE_ENDPOINT=wss://poc2.phala.network/ws
-# Port listend by 'src/server'
-export PORT=5555
-export WHITELISTED_USER_SUFFIX=':phala.network'
-
-# Get it from Riot (https://webapps.stackexchange.com/questions/131056/how-to-get-an-access-token-for-riot-matrix)
-export MATRIX_ACCESS_TOKEN=
-export MATRIX_USER_ID=@example:matrix.org
-
-# Get it from @BotFather
-export TELEGRAM_BOT_TOKEN=
-
-# The endpoint of 'src/server'
-export BACKEND_URL="http://127.0.0.1:${PORT}"
-export SYMBOL=PHA
-# Bot will send the url of a tx by "$EXPLORER_URL/$TXID"
-export EXPLORER_URL=
-
-# Decimals of the native token
-export DECIMALS=12
-
 # Finally start the daemons
 node ./src/bot/index.js &
 node ./src/server/index.js
